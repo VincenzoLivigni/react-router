5fb9df4b03f12f207c55f301014a3b8aaa7a0ec8
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-14 19:43:55 +0100

    Add jumbotron and welcome message to HomePage main

--- src/pagine/HomePage.jsx
+++ src/pagine/HomePage.jsx
@@ -1,3 +1,4 @@
+import Welcome from '../assets/img/photo-1645397925452-dfc04a653d13.avif'
 import Header from "../components/Header";
 
 export default function HomePage() {
@@ -7,7 +8,13 @@ export default function HomePage() {
 
             <main>
                 <>
-                    <h1>HomePage</h1>
+                    <section className='jumbo'>
+                        <img src={Welcome} />
+                    </section>
+                    <section className='benvenuto py-5'>
+                        <h3 className="text-center mb-4">Benvenuto nel nostro sito!</h3>
+                        <h5 className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia libero reiciendi</h5>
+                    </section>
                 </>
             </main>
 
