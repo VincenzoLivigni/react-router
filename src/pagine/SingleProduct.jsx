761c4e8ede9fc47b1c2561a5b9eba88d74d6c96e
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-17 15:14:08 +0100

    Add layouts and styles to the SingleProduct page

--- src/pagine/SingleProduct.jsx
+++ src/pagine/SingleProduct.jsx
@@ -1,3 +1,5 @@
+import Header from "../components/Header";
+import Footer from '../components/Footer';
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import axios from "axios"
@@ -24,10 +26,33 @@ export default function SingleProduct() {
 
     return (
         <>
+            <Header />
+
             <main className="singleProduct pb-5">
-                <h3 className="py-5 text-center"><strong>{singoloProdotto.title}</strong></h3>
+                <h3 className="text-center pt-4 pb-5">{singoloProdotto.title}</h3>
+                <section>
+                    <div className="container pb-5">
+                        <div className="card-content d-flex g-3">
+                            <div className="card-image-top">
+                                <img src={singoloProdotto.image} alt="#" />
+                            </div>
+                            <span className="separatore"></span>
+                            <div className="card-body ms-3">
+                                <span>Product -  <strong>{singoloProdotto.title}</strong></span>
+                                <hr />
+                                <span>Price - <strong>{singoloProdotto.price}$</strong></span>
+                                <hr />
+                                <span>Category - <strong>{singoloProdotto.category}</strong></span>
+                                <hr />
+                                <span>Description - <strong>{singoloProdotto.description}</strong></span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
             </main>
+
+            <Footer />
         </>
     )
 
